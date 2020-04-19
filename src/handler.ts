fe4e57db1e5f08513ee054c7bfda12982f23b10a
Jeffrey Crocker <crockagile@gmail.com>
2020-04-19 18:20:38 -0400

    Fix test types

--- src/handler.ts
+++ src/handler.ts
@@ -1,6 +1,6 @@
 import { HandlerResponse } from './response';
 
-export async function handler(request: Request): Promise<HandlerResponse> {
+export async function handler(_: Request): Promise<HandlerResponse> {
   const hello = 'when tho?';
   const status = 200;
   const body = JSON.stringify({ hello });
