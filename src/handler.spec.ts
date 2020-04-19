63c82afafa7a564ac72d05eb0b938f39b8c9c908
Jeffrey Crocker <crockagile@gmail.com>
2020-04-19 18:20:38 -0400

    Fix test types

--- src/handler.spec.ts
+++ src/handler.spec.ts
@@ -2,7 +2,7 @@ import { expect } from 'chai';
 import { handler } from './handler';
 
 it('handles request', async () => {
-  const { status, body } = await handler();
+  const { status, body } = await handler({} as Request);
   expect(status).equals(200);
   expect(JSON.parse(body)).deep.equals({ hello: 'when tho?' });
 });
