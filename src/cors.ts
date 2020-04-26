792d456d32094afc2de47f8e4ccfc16f235cc537
Jeffrey Crocker <crockagile@gmail.com>
2020-04-26 19:32:54 -0400

    Enable headers

--- src/cors.ts
+++ src/cors.ts
@@ -2,10 +2,7 @@ export async function enableCors(
   responsePromise: Promise<Response>,
 ): Promise<Response> {
   const response = await responsePromise;
-  response.headers.set(
-    'Access-Control-Allow-Headers',
-    'application/json, Content-type',
-  );
+  response.headers.set('Access-Control-Allow-Headers', '*');
   response.headers.set('Access-Control-Allow-Methods', 'GET, POST');
   response.headers.set('Access-Control-Allow-Origin', '*');
   response.headers.set('X-Content-Type-Options', 'nosniff');
