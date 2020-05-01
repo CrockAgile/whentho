73550c6bc59b97fe49623ef9d8e932a491ba5793
Jeffrey Crocker <crockagile@gmail.com>
2020-05-01 09:42:35 -0400

    Fix KV list pagination

--- src/storage/cloudflareKV.ts
+++ src/storage/cloudflareKV.ts
@@ -15,19 +15,22 @@ export class CloudflareKVStorageClient extends StorageClient {
     const key = StorageClient.stringifyKey(item);
     await WhenThoNameSpace.put(key, item.value, { expirationTtl: this.ttl });
   }
-  async getItem(storageKey: StorageKey): Promise<StorageItem | null> {
-    const key = StorageClient.stringifyKey(storageKey);
+  private async getItemByKey(key: string): Promise<StorageItem | null> {
     const value = await WhenThoNameSpace.get(key);
     if (!value) {
       return null;
     }
+    const storageKey = StorageClient.parseKey(key);
     return { ...storageKey, value };
   }
+  async getItem(storageKey: StorageKey): Promise<StorageItem | null> {
+    const key = StorageClient.stringifyKey(storageKey);
+    return this.getItemByKey(key);
+  }
   async listScope(scope: string): Promise<StorageItem[]> {
-    const allItems: StorageItem[] = [];
-
     let list_complete = false;
     let cursor: string | undefined = undefined;
+    const itemMaybePromises = [];
 
     while (!list_complete) {
       // have to use promise intermediary because TS 3.8 has type deduction bug
@@ -38,25 +41,21 @@ export class CloudflareKVStorageClient extends StorageClient {
       const listResult = await listResultPromise;
       const { keys } = listResult;
 
-      const itemsPromise = keys.map(async ({ name }) => {
-        const value = await WhenThoNameSpace.get(name);
-        const storageKey = StorageClient.parseKey(name);
-        return {
-          ...storageKey,
-          value,
-        };
-      });
-
-      const items = await Promise.all(itemsPromise);
-
-      for (const item of items) {
-        if (item.value) {
-          allItems.push(item as StorageItem);
-        }
+      for (const key of keys) {
+        itemMaybePromises.push(this.getItemByKey(key.name));
       }
 
       cursor = listResult.cursor;
       list_complete = listResult.list_complete;
+    }
+
+    const maybeItems = await Promise.all(itemMaybePromises);
+    const allItems = [];
+
+    for (const item of maybeItems) {
+      if (item) {
+        allItems.push(item);
+      }
     }
 
     return allItems;
